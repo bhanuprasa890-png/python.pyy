a8c8a2c2d52ea85a5ebec692ccf36dc199391438
bhanuprasa890-png <bhanuprasa890@gmail.com>
2026-09-20 15:51:49 +0530

    Add files via upload

---
+++ mywork.py
@@ -0,0 +1,147 @@
+#  first question
+print("Hello World")
+# second question
+print("Welcome to Python")
+# third question
+print("Bhanu prasad")
+# fourth question
+print("I am learning python")
+# fifth question
+print("Python programming")
+# sixth question
+print("Good morning")
+# seventh question
+print("Welcome Students")
+# eighth question
+print("Python is easy ")
+# ninth question
+print("My first Program")
+# tenth question
+print("Hello from python!")
+# eleventh question
+print("2+5")
+# twelveth question
+print(2+5)
+# thirteenth question
+print("10-3")
+# fourteenth question
+print(10-3)
+# fifteenth question
+print("Hello world")
+# sixteenth question
+print("python")
+# seventeenth question
+print("Hello")
+# eighteenth question
+print("Hello")
+# nineteenth question
+print("10+20")
+# twentieth question
+print(10+20)
+# twenty first question
+name = "bhanuprasad"
+# twenty second question
+age = 20
+# twenty third question
+marks = 85
+# twenty fourth question
+price = 99.5
+# twenty fifth question
+city = "chennai"
+print(city)
+# twenty sixth question
+score = 95
+print(score)
+# twenty seventh question
+x = 10
+print(x)
+# twenty eighth question
+message = "Wecome"
+print(message)
+# twenty ninth question
+a=10
+b=20
+print(a+b)
+# thirtieth question
+quantity = 5
+price = 10
+print(quantity * price)
+# thirty first question
+x = 25
+print(type(x))
+# thirty second question
+y = 25.5
+print(type(y))
+# thirty third question
+z = "25"
+print(type(z))
+# thirty fourth question
+age = 21
+print(age)
+print(type(age))
+# thirty fifth question
+height = 5.8
+print(height)
+print(type(height))
+# thirty sixth question
+name = "Arun"
+print(name)
+print(type(name))
+# thirty seventh question
+age = 20
+price = 99.5
+name = "Ravi"
+# thirty eighth question
+marks = 90
+print(type(marks))
+# thirty ninth question
+tem = 36.5
+print(type(tem))
+# fortieth question
+message = "Welcome"
+print(type(message))
+# forty first question
+print("Start")
+print("Middle")
+print("End")
+# forty second question
+print("First")
+print("Second")
+print("Third")
+# forty third question
+x = 10
+print(x)
+x = 20
+print(x)
+# forty fourth question
+num = 5
+print(num)
+num = 10
+print(num)
+# forty fifth question
+print("A")
+print("B")
+print("C")
+# forty sixth question
+name = "Ravi"
+age = 20
+print(name)
+print(age)
+# forty seventh question
+print("1")
+print("2")
+print("3")
+print("4")
+# forty eighth question
+x = 5
+y = 10
+print(x)
+print(y)
+# forty ninth question
+print("start")
+print(2+3)
+print("end")
+# fiftieth question
+print("login")
+print("Dashboard")
+print("Logout")
